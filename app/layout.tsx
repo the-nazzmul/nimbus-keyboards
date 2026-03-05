64e0a9e5682d60a34372b4aa4481827ce7f3a924
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-06 03:20:23 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Roboto_Flex } from "next/font/google";
+import "./globals.css";
+
+const robotoFlex = Roboto_Flex({
+  variable: "--font-roboto-flex",
+  subsets: ["latin"],
+  display: "swap",
+  axes: ["wdth", "slnt", "opsz"],
+});
+
+export const metadata: Metadata = {
+  title: "Nimbus Keyboards",
+  description: "Pretty cool keyboards",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${robotoFlex.variable} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/hero.tsx
@@ -0,0 +1,43 @@
+import { Bounded } from "./bounded";
+
+const Hero = () => {
+  return (
+    <section className="blue-gradient-bg relative h-dvh text-white text-shadow-black/30 text-shadow-lg">
+      {/* background image */}
+      <div className="hero-scene pointer-events-none sticky top-0 h-dvh w-full">
+        {/* Canvas */}
+      </div>
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh">
+        <Bounded
+          fullWidth
+          className="absolute inset-x-0 top-18 right-0 md:top-24 md:left-[8vw]"
+        >
+          <h1 className="hero-heading font-black-slanted pl-1 text-6xl leading-[0.8] uppercase sm:text-7xl lg:text-8xl">
+            Built for <br /> the Bold
+          </h1>
+        </Bounded>
+        <Bounded
+          fullWidth
+          className="hero-body absolute inset-x-0 bottom-0 md:right-[8vw] md:left-auto"
+          innerClassName="flex flex-col gap-3"
+        >
+          <div className="max-w-md">
+            <h2 className="font-bold-slanted mb-1 text-4xl uppercase lg:mb-2 lg:text-6xl">
+              Typing Reinvented
+            </h2>
+            <p>
+              Fall in love with the craft thanks to our professional grade
+              keycaps and switches
+            </p>
+          </div>
+          <button className="font-bold-slanted group flex w-fit cursor-pointer items-center gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disabled:grayscale">
+            Buy Vapor75
+            <span className="transition group-hover:translate-x-1">{">"}</span>
+          </button>
+        </Bounded>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;

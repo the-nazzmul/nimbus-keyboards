9590fb8864425e83ccf45518324809d1e51336f0
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2026-03-06 23:34:43 +0600

    added textures and colorspace to keycaps

--- components/keycap.tsx
+++ components/keycap.tsx
@@ -1,10 +1,12 @@
 import * as THREE from "three";
-import { useGLTF } from "@react-three/drei";
+import { Float, useGLTF, useTexture } from "@react-three/drei";
 import { GLTF } from "three-stdlib";
+import { useEffect, useMemo } from "react";
 
 type KeycapProps = {
   position?: [number, number, number];
   rotation?: [number, number, number];
+  texture?: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 };
 
 type GLTFResult = GLTF & {
@@ -17,24 +19,55 @@ type GLTFResult = GLTF & {
 export function Keycap({
   position = [0, 0, 0],
   rotation = [0, 0, 0],
+  texture = 0,
 }: KeycapProps) {
   const { nodes } = useGLTF("/keycap.gltf") as unknown as GLTFResult;
 
+  const textures = [
+    "/keycap_uv-1.png",
+    "/keycap_uv-2.png",
+    "/keycap_uv-3.png",
+    "/keycap_uv-4.png",
+    "/keycap_uv-5.png",
+    "/keycap_uv-6.png",
+    "/keycap_uv-7.png",
+    "/keycap_uv-8.png",
+    "/keycap_uv-9.png",
+  ];
+
+  const uvTexture = textures[texture];
+
+  const keycapTexture = useTexture(uvTexture);
+  // eslint-disable-next-line react-hooks/immutability
+  keycapTexture.colorSpace = THREE.SRGBColorSpace;
+
+  const textureWithFlip = useMemo(() => {
+    const t = keycapTexture.clone();
+    t.flipY = false;
+    return t;
+  }, [keycapTexture]);
+
+  useEffect(() => {
+    return () => textureWithFlip.dispose();
+  }, [textureWithFlip]);
+
   const placeholderMat = new THREE.MeshStandardMaterial({
-    color: "#cccccc",
-    roughness: 0.2,
+    map: textureWithFlip,
+    roughness: 0.7,
   });
 
   return (
-    <group dispose={null} position={position} rotation={rotation}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.Keycap.geometry}
-        material={placeholderMat}
-        rotation={[Math.PI / 2, 0, 0]}
-        scale={10}
-      />
-    </group>
+    <Float rotationIntensity={3} position={position} rotation={rotation}>
+      <group dispose={null}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.Keycap.geometry}
+          material={placeholderMat}
+          rotation={[Math.PI / 2, 0, 0]}
+          scale={10}
+        />
+      </group>
+    </Float>
   );
 }
--- components/scene.tsx
+++ components/scene.tsx
@@ -24,16 +24,16 @@ const Scene = () => {
         rotation={[1.6, 0.4, 0]}
       />
       <group>
-        <Keycap position={[0, -0.4, 2.6]} />
-        <Keycap position={[-1.4, 0, 2.3]} />
-        <Keycap position={[-1.8, 1, 1.5]} />
-        <Keycap position={[0, 1, 1]} />
-        <Keycap position={[0.7, 0.9, 1.4]} />
-        <Keycap position={[1.3, -0.3, 2.3]} />
-        <Keycap position={[0, 1, 2]} />
-        <Keycap position={[-0.7, 0.6, 2]} />
-        <Keycap position={[0.77, 1, 2.8]} />
-        <Keycap position={[2, 0, 1]} />
+        <Keycap position={[0, -0.4, 2.6]} rotation={[0, 2, 3]} texture={0} />
+        <Keycap position={[-1.4, 0, 2.3]} rotation={[3, 2, 1]} texture={1} />
+        <Keycap position={[-1.8, 1, 1.5]} rotation={[0, 1, 3]} texture={2} />
+        <Keycap position={[0, 1, 1]} rotation={[0, 4, 2]} texture={3} />
+        <Keycap position={[0.7, 0.9, 1.4]} rotation={[3, 2, 0]} texture={4} />
+        <Keycap position={[1.3, -0.3, 2.3]} rotation={[1, 2, 0]} texture={5} />
+        <Keycap position={[0, 1, 2]} rotation={[2, 2, 3]} texture={6} />
+        <Keycap position={[-0.7, 0.6, 2]} rotation={[1, 4, 0]} texture={7} />
+        <Keycap position={[-0.77, 0.1, 2.8]} rotation={[3, 2, 3]} texture={8} />
+        <Keycap position={[2, 0, 1]} rotation={[0, 0, 3]} texture={7} />
       </group>
       <Environment
         files={["/hdr/blue-studio.hdr"]}
